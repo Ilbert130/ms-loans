d9cb0eef6d7d4c2e3a01a09e98efc7408a0956fb
Ilbert130 <ilbertca27@gmail.com>
2025-09-08 15:41:59 -0400

    Loans entity created

---
+++ src/main/java/org/icadev/loans/entity/Loans.java
@@ -0,0 +1,37 @@
+package org.icadev.loans.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "loans")
+@Getter
+@Setter
+@ToString(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loans extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @Column(name = "loan_id")
+    private Long loanId;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "loan_number")
+    private String loanNumber;
+
+    @Column(name = "loan_type")
+    private String loanType;
+
+    @Column(name = "total_loan")
+    private Long totalLoan;
+
+    @Column(name = "amount_paid")
+    private Long amountPaid;
+
+    @Column(name = "outstanding_amoun")
+    private Long outstandingAmount;
+}
