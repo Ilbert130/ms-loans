dfed0e7ec0ebd693f305a6cfc08e3a208c6a3353
Ilbert130 <ilbertca27@gmail.com>
2025-09-08 15:18:17 -0400

    First configuration made

---
+++ src/test/java/org/icadev/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package org.icadev.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
